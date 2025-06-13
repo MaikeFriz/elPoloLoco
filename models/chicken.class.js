2cc7b75df226305e80d983f977c40a2ae0dca00c
Maike Friz <you@example.com>
2025-06-13 11:39:05 +0700

    added basic classes

---
+++ models/chicken.class.js
@@ -0,0 +1,7 @@
+class Chicken extends MovableObject{
+
+    constructor() {
+        super().loadImage('../assets/img_pollo_locco/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+    }
+
+}
