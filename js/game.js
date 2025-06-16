10e06329ab82ddc43811f9f44bc590e7e1eb8538
Maike Friz <you@example.com>
2025-06-16 13:39:45 +0700

    added requestAnimationFrame and displayed chickens

--- js/game.js
+++ js/game.js
@@ -1,10 +1,9 @@
-let canvas;
 let ctx;
-
-let world = new World();
+let world;
 
 function init() {
     canvas = document.getElementById("canvas");
+    world = new World(canvas);
     ctx = canvas.getContext("2d");
     console.log('My character is', world.character);
 }
